6c3025bcee7ce81519ab724fce58d50d99dbe28a
Hui <xuhuizhu369@gmail.com>
2021-01-15 05:17:15 -0800

    cleans up userResolver logic

--- Server/Resolvers/userResolver.js
+++ Server/Resolvers/userResolver.js
@@ -1,4 +1,4 @@
-const users = require('../assets/users.js')
+
 const { uModel } = require('../../DB/model.js')
 
 const usersResolver = async () => await uModel.find().then(allUsers => allUsers)
@@ -8,20 +8,7 @@ const userResolver = async (p, args) => await uModel.findOne({ name: args.name }
 
 const addUserResolver = async (src, args) => {
 
-    const username = args.name
-    const newUser = new uModel({ name: username })
-
-    // 1.14.21 ***
-    // user is saving, but in attempting to return info to client
-    // client isn't able to parse it displays this error: 
-
-    // "Unhandled Rejection (Error): Cannot read property 'catch' of undefined"
-
-    // seems like newUser.save().then().catch()
-    // when I remove .catch() block it fixes this issue
-
-    // [0] clean - up clientside render, logic looks jank af
-    // [1] see what the data looks like coming back to FE
+    const newUser = new uModel({ name: args.name })
 
     await newUser.save()
 
